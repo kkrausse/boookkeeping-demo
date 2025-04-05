49cf72fd6f5a3714eea0196817a4a9e039ac4e1d
kevin.krausse <kevin.krausse@taxbit.com>
2025-04-05 16:59:27 -0700

    transaction model

--- bookkeeping-backend/db/schema.rb
+++ bookkeeping-backend/db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_05_235039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "transactions", force: :cascade do |t|
+    t.string "description"
+    t.string "category"
+    t.decimal "amount"
+    t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
